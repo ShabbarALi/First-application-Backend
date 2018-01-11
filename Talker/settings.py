194c2758063605597d1911142c5974d74bc7c5af
ShabbarAli <shabbar.crewlogix@gmail.com>
2018-01-11 07:34:57 +0500

    Sql111

--- Talker/settings.py
+++ Talker/settings.py
@@ -108,7 +108,7 @@ DATABASES = {
         'USER': 'ShabbarALi',
         'PASSWORD': 'Ghulam151214',
         'HOST': 'ShabbarALi.mysql.pythonanywhere-services.com',
-        'PORT' : '3306',
+        'PORT' : '',
     }
 }
 
